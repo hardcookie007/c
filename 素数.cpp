44efc1ebc68c4d35ee3862055d4c8252587ae3fc
hardcookie007 <3237417169@qq.com>
2025-11-06 22:43:44 +0800

    Update 素数.cpp

--- 素数.cpp
+++ 素数.cpp
@@ -5,7 +5,7 @@ int main(void)
 	int prime[28];
 	int i;
 	int x;
-	for(i=0;i<28;i++)
+	for(i=0;i<28;i++)    
 	prime[i]=1;
 	
 	for(x=2;x<28;x++)
@@ -24,3 +24,6 @@ int main(void)
 	printf("\n"); 
 	return 0;
 }
+
+//临摹翁恺老师8.2.2
+//太难了，第一次听完课，看着源代码都写不出来，不敢想我以后怎么自己写代码啊（发愁）
